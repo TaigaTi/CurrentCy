4e14e56c5843bda78984a5be0f48880959848906
Tarika Birch <tarikabirch@gmail.com>
2025-09-15 15:16:26 -0400

    Converted CurrentCy_UI from submodule to regular folder

--- CurrentCy_API/src/index.ts
+++ CurrentCy_API/src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
 import currencyRoutes from './routes/currencyRoutes';
+import cors from 'cors';
 
 const app = express();
 const prisma = new PrismaClient();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
 // Routes
